c30cc76c6c2302e43337735dec5ebf27c27ba06a
Matheus Inácio BS <matheus.bs@ic.ufal.br>
2018-02-01 13:59:46 -0300

    start kriging

--- app/src/main/java/laccan/cloud/prediction/KrigingPrediction.java
+++ app/src/main/java/laccan/cloud/prediction/KrigingPrediction.java
@@ -1,4 +1,31 @@
 package laccan.cloud.prediction;
 
-public class KrigingPrediction {
+
+import laccan.devices.MicazMsg;
+import laccan.devices.helper.utils.Pair;
+import smile.interpolation.KrigingInterpolation;
+
+import java.util.ArrayList;
+
+public class KrigingPrediction extends Predictor {
+    KrigingInterpolation interpolator;
+    double[][] x;
+    double[] y;
+
+    public void train() {
+        normalize();
+        interpolator = new KrigingInterpolation(x, y);
+    }
+
+    public ArrayList<Pair<String, Double>> result(ArrayList<MicazMsg> test) {
+        return null;
+    }
+
+    public Pair<Double, Double> mse() {
+        return null;
+    }
+
+    private void normalize() {
+
+    }
 }
--- app/src/main/java/laccan/main/PersistenceApp.java
+++ app/src/main/java/laccan/main/PersistenceApp.java
@@ -1,4 +1,7 @@
 package laccan.main;
 
 public class PersistenceApp {
+    public static void main(String[] args) {
+
+    }
 }
